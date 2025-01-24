50afc038caba4633029434af43b5df45a29ab0e6
vicse <vicseore@gmail.com>
2025-01-24 15:31:35 -0500

    feat: NATS server config added and changing TCP microservices communication for NATS

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { envs } from './config/envs';
+import { envs } from './config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port ?? 3000,
+        servers: envs.natsServers,
       },
     },
   );
 
-  // app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       transformOptions: {
